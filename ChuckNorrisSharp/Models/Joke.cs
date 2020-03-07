c25aef53738cc265aea2aeb4a7b855b62f2c085d
Kevin Quan <kevin.quan@hotmail.ca>
2020-03-06 17:09:12 -0700

    Add Joke model

---
+++ ChuckNorrisSharp/Models/Joke.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChuckNorrisSharp.Models
+{
+    /// <summary>
+    /// Represents a Chuck Norris factoid.
+    /// </summary>
+    class Joke
+    {
+        public List<object> Categories { get; set; }
+        public string CreatedAt { get; set; }
+        public string IconUrl { get; set; }
+        public string Id { get; set; }
+        public string UpdatedAt { get; set; }
+        public string Url { get; set; }
+        public string Value { get; set; }
+    }
+}
